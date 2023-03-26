71da03f46140753523aafa267d67d057006f3afa
Martinus Werts <martinus.werts@ens-rennes.fr>
2023-03-26 12:51:27 +0200

    test/example code for air-equilibrated pH calculation

--- src/test_air_equilibrated.py
+++ src/test_air_equilibrated.py
@@ -3,9 +3,10 @@ from pHcalc import Acid as AcidAq
 from pHcalc import System
 from pHcalc.air_equilibrated import SystemAirEquilibrated
 
-# Test code for AirEquilibrated
 
-# 1. test transfer of kwargs from sub to super
+# Test code for AirEquilibrated module
+
+# 1. Test transfer of kwargs from sub to super
 sys0 = System()
 syst = SystemAirEquilibrated()
 assert sys0.Kw == syst.Kw
@@ -15,58 +16,81 @@ syst = SystemAirEquilibrated(Kw=testKw)
 assert syst.Kw == testKw
 
 
-# 2. benchmark calculations
+print()
 
-# TO DO make the following a loop over
-#       a dictionary or over list of dictionaries, 
-#       include benchmark data from Aqion/Phreeqc 
+
+# 2. Simple exazmple
+
+sys0 = System()
+sys0.pHsolve()
+print('pure water, closed system                     : pH = ', sys0.pH)
+
+syst = SystemAirEquilibrated()
+syst.DICsolve()
+print('pure water, air-equilibrated system (in 2021) : pH = ', syst.pH)
+
+syst1972 = SystemAirEquilibrated(P_CO2 = 372.46e-6)
+syst1972.DICsolve()
+print('pure water, air-equilibrated system (in 1972) : pH = ', syst1972.pH)
+
+print()
+
+
+# 3. Benchmark/example calculations
+
+# As benchmark calculations we used the on-line version of aqion which uses
+# PHREEQC as a back-end for calculations.
+#   https://www.aqion.onl/
+#   https://www.usgs.gov/software/phreeqc-version-3
+# The on-line results are identical to those obtained with the Windows version
+# of aqion.
+# Reference temperature: 298 K
 #
-hcl = IonAq(charge=-1, conc=0.1)
-syst = SystemAirEquilibrated(hcl)
-syst.DICsolve()
-print('0.1M HCl in equilibrium with atmosphere pH = ', syst.pH,
-      ' DIC = ', syst.DIC, 'M')
-print()    
+# At higher ionic strengths, deviations appear between the values calculated by
+# pHcalc and those from aqion/PHREEQC. This is due to the fact that PHREEQC
+# uses activities and ionic strength-dependent equilibrium constants.
+# pHcalc uses simple mass-action law with constant equilibrium constants and
+# actual concentrations
 
 
-hcl = IonAq(charge=-1, conc=0.001)
-syst = SystemAirEquilibrated(hcl)
-syst.DICsolve()
-print('1 mM HCl in equilibrium with atmosphere pH = ', syst.pH,
-      ' DIC = ', syst.DIC, 'M')
-print()    
+# CO2 value used by aqion (watch out: aqion uses pCO2, not partial pressure)
+aqionP_CO2 = 10**-3.408 # conversion of pCO2 into P_CO2 [atm]
 
+# Test cases
+paramlist = [
+    {'descr':       '0.1 M HCl(aq) in eq. with amosphere',
+     'ioncharge':  -1,
+     'ionconc':     0.1,
+     'aqion_pH':    1.08},
+    {'descr':       '1 mM HCl(aq) in eq. with amosphere',
+     'ioncharge':  -1,
+     'ionconc':     0.001,
+     'aqion_pH':    3.02},
+    {'descr':       'pure water in eq. with amosphere',
+     'ioncharge':   None,
+     'ionconc':     None,
+     'aqion_pH':    5.61},
+    {'descr':       '1 mM NaOH(aq) in eq. with amosphere',
+     'ioncharge':   1,
+     'ionconc':     0.001,
+     'aqion_pH':    8.20},
+    {'descr':       '10 mM NaOH(aq) in eq. with amosphere',
+     'ioncharge':   1,
+     'ionconc':     0.01,
+     'aqion_pH':    9.11},
+    {'descr':       '0.1 M NaOH(aq) in eq. with amosphere',
+     'ioncharge':   1,
+     'ionconc':     0.1,
+     'aqion_pH':    9.71},
+    ]
 
-pure_aireq = SystemAirEquilibrated()
-pure_aireq.DICsolve()
-print('pure water in equilibrium with atmosphere pH = ', pure_aireq.pH,
-      ' DIC = ', pure_aireq.DIC, 'M')
-print()
-
-naoh = IonAq(charge=1, conc=0.001)
-syst = SystemAirEquilibrated(naoh)
-syst.DICsolve()
-print('1 mM NaOH in equilibrium with atmosphere pH = ', syst.pH,
-      ' DIC = ', syst.DIC, 'M')
-print()
-
-naoh = IonAq(charge=1, conc=0.01)
-syst = SystemAirEquilibrated(naoh)
-syst.DICsolve()
-print('0.01M NaOH in equilibrium with atmosphere pH = ', syst.pH,
-      ' DIC = ', syst.DIC, 'M')
-print()
-
-naoh = IonAq(charge=1, conc=0.1)
-syst = SystemAirEquilibrated(naoh)
-syst.DICsolve()
-print('0.1M NaOH in equilibrium with atmosphere pH = ', syst.pH,
-      ' DIC = ', syst.DIC, 'M')
-print()
-
-
-pure_aireq = SystemAirEquilibrated(P_CO2 = 320e-6)
-pure_aireq.DICsolve()
-print('pure water in equilibrium with atmosphere (1972) pH = ', pure_aireq.pH,
-      ' DIC = ', pure_aireq.DIC, 'M')
-print()
+for param in paramlist:
+    if param['ionconc'] is None:
+        syst = SystemAirEquilibrated(P_CO2 = aqionP_CO2)
+    else:
+        ion = IonAq(charge = param['ioncharge'],
+                    conc = param['ionconc'])
+        syst = SystemAirEquilibrated(ion, P_CO2 = aqionP_CO2)
+    syst.DICsolve()
+    print('{0:40s} | DIC = {1:7.3e} M | pH = {2:5.2f} [aqion: {3:5.2f}]'.\
+          format(param['descr'], syst.DIC, syst.pH, param['aqion_pH']))
